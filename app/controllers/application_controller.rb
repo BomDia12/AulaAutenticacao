e76218866e6614e722c9e9dbdb725ad1cc97a7fb
BomDia12 <chamatico2@gmail.com>
2021-10-01 16:15:03 -0300

    :bug: :(
    :(

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
   def authentication_failure
-    render json: { message: 'Não conseguimos efetuar o seu login!' }, status: :unauthorized
+    render json: { message: 'Não conseguimos efetuar o seu login! :(' }, status: :unauthorized
   end
 end
